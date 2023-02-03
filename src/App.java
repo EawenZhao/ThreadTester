9ac8fe926da7dff3739bd49a0e38c962942d1975
zyh19 <zyh1999zyh@yeah.net>
2023-02-03 16:31:19 +0800

    111

--- src/App.java
+++ src/App.java
@@ -9,6 +9,7 @@ public class App {
 
     public static void main(String[] args) throws InterruptedException {
         Thread[] threads = new Thread[num];
+
         for (int i = 0; i < num; i++) {
             Login login = new Login();
             login.setLoginName("" + String.format("%2s", (i + 1)) + "号用户");
